01f2f1027c4aa178cfd3dbdd0580833bc65804a6
Mark Nagibin <myrkwill@icloud.com>
2022-10-15 04:22:17 +0500

    Add request params

--- src/main/java/ru/myrkwill/springmvcapp/conrollers/FirstController.java
+++ src/main/java/ru/myrkwill/springmvcapp/conrollers/FirstController.java
@@ -3,24 +3,29 @@ package ru.myrkwill.springmvcapp.conrollers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Mark Nagibin
  */
 
 @Controller
-@RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
-        System.out.println("HELLO!");
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodbyePage() {
-        System.out.println("GOODBYE!");
+    public String goodbyePage(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("Goodbye, " + name + " " + surname);
         return "first/goodbye";
     }
 }
