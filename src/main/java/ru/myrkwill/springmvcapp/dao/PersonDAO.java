87f910372fe87001745c78ef867fca308f0e1c62
Mark Nagibin <myrkwill@icloud.com>
2022-10-15 06:52:52 +0500

    CRUD, REST and DAO

---
+++ src/main/java/ru/myrkwill/springmvcapp/dao/PersonDAO.java
@@ -0,0 +1,33 @@
+package ru.myrkwill.springmvcapp.dao;
+
+import org.springframework.stereotype.Component;
+import ru.myrkwill.springmvcapp.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Mark Nagibin
+ */
+@Component
+public class PersonDAO {
+
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mark"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
